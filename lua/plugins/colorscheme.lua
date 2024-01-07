3bb6d96b024835f68ca991c79ebbc1ec9e561032
Felipe Alencar <felipealencardev@gmail.com>
2024-01-07 16:32:25 -0300

    add onedark theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,16 @@
 return {
-  'craftzdog/solarized-osaka.nvim',
-  lazy = true,
-  priority = 1000,
-  opts = {},
+  {
+    'craftzdog/solarized-osaka.nvim',
+    lazy = true,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    -- Theme inspired by Atom
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'onedark'
+    end,
+  },
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -15,6 +15,19 @@ return {
     end
   },
 
+  -- lualine
+  {
+    'nvim-lualine/lualine.nvim',
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'onedark',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+
   -- filename
   {
     "b0o/incline.nvim",
