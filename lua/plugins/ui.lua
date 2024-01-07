f3f910dd03de22af82b361d7fea962474623024d
Felipe Alencar <felipealencardev@gmail.com>
2024-01-07 14:00:01 -0300

    change lualine colorscheme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,14 +1,28 @@
 return {
-  'folke/noice.nvim',
-  opts = function(_, opts)
-    table.insert(opts.routes, {
-      filter = {
-        event = 'notify',
-        find = 'No information available',
-      },
-      opts = { skip = true },
-    })
+  -- noice
+  {
+    'folke/noice.nvim',
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = 'notify',
+          find = 'No information available',
+        },
+        opts = { skip = true },
+      })
 
-    opts.presets.lsp_doc_border = true
-  end
+      opts.presets.lsp_doc_border = true
+    end
+  },
+
+  -- lualine
+  {
+    'nvim-lualine/lualine.nvim',
+    event = 'VeryLazy',
+    opts = {
+      options = {
+        theme = 'solarized-dark'
+      }
+    }
+  }
 }
