8782ffda24780f50076b365dc2afd87f02211e72
Felipe Alencar <felipealencardev@gmail.com>
2024-01-07 14:59:11 -0300

    add lsp and treesitter files

---
+++ lua/config/treesitter.lua
@@ -0,0 +1,35 @@
+return {
+  { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "astro",
+        "cmake",
+        "cpp",
+        "css",
+        "gitignore",
+        "go",
+        "graphql",
+        "http",
+        "java",
+        "rust",
+        "scss",
+        "sql",
+        "svelte",
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+
+      -- MDX
+      vim.filetype.add({
+        extension = {
+          mdx = "mdx",
+        },
+      })
+      vim.treesitter.language.register("markdown", "mdx")
+    end,
+  },
+}
