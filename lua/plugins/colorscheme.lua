a94608d753910d692652122597c6af04543159f6
Felipe Alencar <felipealencardev@gmail.com>
2024-01-09 18:55:44 -0300

    remove solarized-osaka theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,5 @@
 return {
   {
-    'craftzdog/solarized-osaka.nvim',
-    lazy = true,
-    priority = 1000,
-    opts = {},
-  },
-  {
     -- Theme inspired by Atom
     'navarasu/onedark.nvim',
     priority = 1000,
